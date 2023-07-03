932e1ef516d98b1afa27d147fb97520220d7519b
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-07-02 22:57:22 -0600

    Created constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,19 @@
+/// Written by Juan Pablo Gutiérrez
+/// 02 - 07 - 2023
+
+import 'package:flutter/material.dart';
+
+/* Widget configuration */
+const double standartSpacerHeight = 20;
+
+/* Text Configuration */
+const String fontFamily = "Manrope";
+
+const TextStyle defaultStyle = TextStyle();
+
+const TextStyle bigTextStyle = TextStyle(
+  fontFamily: fontFamily,
+  fontWeight: FontWeight.bold,
+  color: const Color(0xffFFFFFF),
+  fontSize: 52,
+);
