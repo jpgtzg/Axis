a075713a229ef57e220a67402ec0cab94bb804f5
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-07-04 13:57:16 -0600

    Added palette to constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -14,6 +14,28 @@ const TextStyle defaultStyle = TextStyle();
 const TextStyle bigTextStyle = TextStyle(
   fontFamily: fontFamily,
   fontWeight: FontWeight.bold,
-  color: const Color(0xffFFFFFF),
+  color: Colors.white,
   fontSize: 52,
+);
+
+/* Color configuration */
+const Color paletePink = Color(0xffFF128E);
+const Color paletePurple = Color(0xff8310DE);
+const Color paleteBlue = Color(0xff1D3BF4);
+const Color paleteLightBlue = Color(0xff10CCDE);
+const Color paleteBlack = Color(0xff000000);
+
+const LinearGradient backGroundGradient = LinearGradient(
+  begin: Alignment.topRight,
+  end: Alignment.bottomLeft,
+  colors: [
+    paleteLightBlue,
+    paleteBlue,
+  ],
+);
+
+const Icon menuIcon = Icon(
+  Icons.menu,
+  color: Colors.white,
+  size: 42,
 );
