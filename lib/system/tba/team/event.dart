b66a248ff6c3c4d6c7d27a9f10046ede6a95bd5f
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-07-05 12:19:30 -0600

    Created Event class

---
+++ lib/system/tba/team/event.dart
@@ -0,0 +1,33 @@
+/// Written by Juan Pablo Gutiérrez
+/// 04 - 07 - 2023
+/// Represents an entire event
+
+class Event {
+  final String name;
+
+  final String startDate;
+  final String endDate;
+  final String eventType;
+  final String eventKey;
+
+  Event(
+      {required this.name,
+      required this.startDate,
+      required this.endDate,
+      required this.eventType,
+      required this.eventKey});
+
+  @override
+  String toString() {
+    return "Name: $name";
+  }
+
+  factory Event.fromJson(Map<String, dynamic> jsonEvent) {
+    return Event(
+        name: jsonEvent["name"] as String,
+        startDate: jsonEvent["start_date"] as String,
+        endDate: jsonEvent["end_date"] as String,
+        eventType: jsonEvent["event_type_string"] as String,
+        eventKey: jsonEvent["event_code"] as String);
+  }
+}
