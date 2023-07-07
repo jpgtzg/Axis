e1cc9258e389900c3f48e4b01d0f37102ce8c331
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-07-06 18:33:54 -0600

    Cleaned up regional card and improved looks

--- lib/widgets/regional_card.dart
+++ lib/widgets/regional_card.dart
@@ -1,9 +1,10 @@
 /// Written by Juan Pablo Gutiérrez
 /// 05 - 07 - 2023
 import 'package:axis/constants.dart';
+import 'package:axis/screens/regional_screen.dart';
 import 'package:flutter/material.dart';
 
-import '../system/tba/team/event.dart';
+import '../system/tba/event/event.dart';
 
 class RegionalCard extends StatelessWidget {
   final Event event;
@@ -11,16 +12,32 @@ class RegionalCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width / (1 / 8),
-      decoration: BoxDecoration(
-        color: Colors.red,
-        borderRadius: BorderRadius.circular(30),
-      ),
-      child: Center(
-        child: Text(
-          event.name,
-          style: defaultStyle,
+    return GestureDetector(
+      onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) =>  RegionalScreen(event: event))),
+      child: Container(
+        width: MediaQuery.of(context).size.width / (1 / 8),
+        decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+               Text(
+                (DateTime.parse(event.startDate).isAfter(DateTime.now()))
+                    ? "Next event:"
+                    : "Past event:",
+                style: substitleStyle,
+              ),
+              Text(
+                event.name,
+                style: defaultStyle,
+                overflow: TextOverflow.visible,
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
         ),
       ),
     );
