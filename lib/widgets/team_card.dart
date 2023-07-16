2f516eb310d0267c1145f75c0679a77ad9b6f26e
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-07-15 23:21:28 -0600

    Redesigned team_card

--- lib/widgets/team_card.dart
+++ lib/widgets/team_card.dart
@@ -1,6 +1,7 @@
 /// Written by Juan Pablo Gutiérrez
 /// 12 - 07 - 2import 'package:flutter/material.dart';
 ///
+import 'package:axis/constants.dart';
 import 'package:flutter/material.dart';
 import '../system/tba/team/team.dart';
 
@@ -12,16 +13,39 @@ class TeamCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      clipBehavior: Clip.hardEdge,
       width: MediaQuery.of(context).size.height / 2.5,
       height: MediaQuery.of(context).size.height / 2.5,
       decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(team.imgUrl ?? ""),
+          fit: BoxFit.cover,
+        ),
         color: Colors.red,
         borderRadius: BorderRadius.circular(30),
       ),
-      child: Image.network(
-        team.imgUrl ?? "",
-        fit: BoxFit.cover,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
+        child: Align(
+          alignment: Alignment.bottomLeft,
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              return Container(
+                alignment: Alignment.center,
+                height: constraints.maxHeight / 3.5,
+                width: constraints.maxWidth / 1.5,
+                decoration: BoxDecoration(
+                  color: paletePink
+                      .withAlpha(240), // Theme.of(context).highlightColor,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Text(
+                  team.teamNumber,
+                  style: defaultStyle,
+                ),
+              );
+            },
+          ),
+        ),
       ),
     );
   }
