f54baf1e4dc76dbf13e67baba0149b1f358ab818
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-07-26 17:43:21 -0600

    Expaneded color palette

--- lib/constants.dart
+++ lib/constants.dart
@@ -60,6 +60,15 @@ const Color paleteBlue = Color(0xff1D3BF4);
 const Color paleteLightBlue = Color(0xff10CCDE);
 const Color paleteBlack = Color(0xff000000);
 
+const Color paleteGreen = Color(0xFF00FF7F);
+const Color paleteTeal = Color(0xFF00FFFF);
+const Color paleteYellow = Color(0xFFFFFF00);
+const Color paleteLighGreen = Color(0xFF90EE90);
+const Color paleteOrange = Color(0xFFFFA500);
+const Color paleteCoral = Color(0xFFFF7F50);
+const Color paleteDarkOrange = Color(0xFFFF8C00);
+const Color paleteDarkColar = Color(0xFFFF4500);
+
 const LinearGradient backGroundGradient = LinearGradient(
   begin: Alignment.topRight,
   end: Alignment.bottomLeft,
