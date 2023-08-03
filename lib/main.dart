cfd3046aeaa2ff5b79751625aa5c1856fc186b47
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-08-03 10:44:04 -0600

    Created realm connection

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@
 import 'package:axis/screens/home_screen.dart';
 import 'package:axis/system/api_manager.dart';
 import 'package:axis/system/axis/realm/_MatchSchema.dart';
+import 'package:axis/system/axis/realm/realm_manager.dart';
+import 'package:axis/system/tba/system_constants.dart';
 import 'package:flutter/material.dart';
 import 'package:realm/realm.dart';
 
@@ -14,7 +16,45 @@ import 'constants.dart';
 void main() async {
   setClient();
 
+  final appConfig = AppConfiguration(appID);
+  final app = App(appConfig);
 
+  final user = await app.logIn(Credentials.anonymous());
+  final realmConfig = Configuration.flexibleSync(
+    user,
+    [MatchSchema.schema],
+  );
+
+  late Realm realm;
+  if (await isDeviceOnline()) {
+    realm = await Realm.open(realmConfig);
+  } else {
+    realm = Realm(realmConfig);
+  }
+  // Check if the subscription already exists before adding
+  /*  final userTodoSub = realm.subscriptions.findByName('getMatchData');
+  if (userTodoSub == null) {
+    realm.subscriptions.update((mutableSubscriptions) {
+      // server-side rules ensure user only downloads their own Todos
+      mutableSubscriptions.add(realm.all<MatchSchema>(), name: 'getMatchData');
+    });
+  } */
+
+  final match = MatchSchema(
+    ObjectId(),
+  );
+
+  realm.write(() {
+    realm.add(match);
+  });
+
+/*   var r = await getTeamEvents(2023, true);
+ */ /* var x = await getFullEPA("4635");
+  print(x!.end); */
+/*   r!.forEach((element) => print(element.name));
+ */
+/*   getImageUrl(1678.toString(), 2022.toString());
+ */
   runApp(const AxisApp());
 }
 
