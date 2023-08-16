2269ee37f2e0d841ada156f56b924f222d8f0a4b
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-08-15 22:01:14 -0600

    Expanded realm_manager

--- lib/system/axis/realm/realm_manager.dart
+++ lib/system/axis/realm/realm_manager.dart
@@ -1,8 +1,43 @@
 /// Written by Juan Pablo Gutiérrez
 /// 3 - 08 - 2023
 
+import 'package:axis/system/api_manager.dart';
+import 'package:axis/system/axis/realm/realm_models.dart';
+import 'package:realm/realm.dart';
+
+import '../../tba/system_constants.dart';
+
+Realm? realm;
+
+void setRealm(Configuration realmConfig) async {
+  final appConfig = AppConfiguration(appID);
+  final app = App(appConfig);
+
+  final user = await app.logIn(Credentials.anonymous());
+  final realmConfig = Configuration.flexibleSync(
+      user,
+      [
+        MatchSchema.schema,
+        Question.schema,
+        MatchFormSettingsSchema.schema,
+      ],
+      clientResetHandler:
+          ManualRecoveryHandler((clientResetError) => print(clientResetError)));
+
+  if (await isDeviceOnline()) {
+    realm = await Realm.open(realmConfig);
+  } else {
+    realm = Realm(realmConfig);
+  }
+}
+
+void write(var addFunction) async {
+  if (realm == null) setClient();
+  realm?.write(() => addFunction);
+}
+
 Future<bool> isDeviceOnline() async {
   //TODO ADD LOGIC
-  
+
   return true;
 }
