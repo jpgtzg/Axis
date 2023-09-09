638e44fa22038b425a2dca1fa1ad7df06342b740
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-09-09 12:00:00 -0600

    Removed debug code

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:axis/screens/home_screen.dart';
 import 'package:axis/system/api_manager.dart';
 import 'package:axis/system/axis/realm/realm_manager.dart';
 import 'package:axis/system/axis/realm/realm_models.dart';
+import 'package:axis/system/tba/system_constants.dart';
 import 'package:flutter/material.dart';
 import 'package:realm/realm.dart';
 
@@ -16,20 +17,6 @@ void main() async {
   setClient();
   await setRealm();
 
-  MatchFormSettingsSchema matchFormSettingsSchema = MatchFormSettingsSchema(ObjectId(), 26);
-  
-  matchFormSettingsSchema.questionsArray.add(Question("Numero de equipo", "String input")); 
-  matchFormSettingsSchema.questionsArray.add(Question("Nombre de equipo", "String input")); 
-  matchFormSettingsSchema.questionsArray.add(Question("Puntos de equipo", "Int input")); 
-  matchFormSettingsSchema.questionsArray.add(Question("Puntos en auto de equipo", "Int input")); 
-  matchFormSettingsSchema.questionsArray.add(Question("Puntos en teleop de equipo", "Int input")); 
-  matchFormSettingsSchema.questionsArray.add(Question("Puntos en endgame de equipo", "Int input")); 
-  matchFormSettingsSchema.questionNumber = matchFormSettingsSchema.questionsArray.length;
-
-  updateMatchForm(matchFormSettingsSchema);
-
-/*   write(matchFormSettingsSchema);
- */
   runApp(const AxisApp());
 }
 
