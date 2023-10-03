67529391f11eb3f16d54ced4d7375864605b1ea6
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-10-02 20:23:25 -0600

    Created dashboard screens

---
+++ lib/screens/match_scouting/match_dashboard_screen.dart
@@ -0,0 +1,13 @@
+/// Written by Juan Pablo Gutierrez
+/// 02 10 2023
+
+import 'package:flutter/material.dart';
+
+class MatchDashboardScreen extends StatelessWidget {
+  const MatchDashboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/screens/pit_scouting/pit_dashboard_screen.dart
@@ -0,0 +1,13 @@
+/// Written by Juan Pablo Gutiérrez
+/// 02 01 2023
+
+import 'package:flutter/material.dart';
+
+class PitDashboardScreen extends StatelessWidget {
+  const PitDashboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
