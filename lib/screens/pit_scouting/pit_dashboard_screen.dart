e70b49994224967d9c7c9cffd33322acc7f22494
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-10-15 23:00:37 -0600

    Added requirements

--- lib/screens/pit_scouting/pit_dashboard_screen.dart
+++ lib/screens/pit_scouting/pit_dashboard_screen.dart
@@ -1,10 +1,15 @@
 /// Written by Juan Pablo Gutiérrez
 /// 02 01 2023
 
+import 'package:axis/system/tba/event/event.dart';
+import 'package:axis/system/tba/team/team.dart';
 import 'package:flutter/material.dart';
 
 class PitDashboardScreen extends StatelessWidget {
-  const PitDashboardScreen({super.key});
+  final Event event;
+  final Team team;
+  const PitDashboardScreen(
+      {required this.team, required this.event, super.key});
 
   @override
   Widget build(BuildContext context) {
