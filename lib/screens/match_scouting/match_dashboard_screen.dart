19d8c52f95953ee1e05c3faa7c9fbbdec027999c
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-10-30 12:00:00 -0600

    Expanded dashboard screen

--- lib/screens/match_scouting/match_dashboard_screen.dart
+++ lib/screens/match_scouting/match_dashboard_screen.dart
@@ -1,6 +1,7 @@
 /// Written by Juan Pablo Gutierrez
 /// 02 10 2023
 
+import 'package:axis/system/axis/Data.dart';
 import 'package:axis/system/axis/realm/realm_manager.dart';
 import 'package:axis/system/tba/event/event.dart';
 import 'package:axis/system/tba/team/team.dart';
@@ -12,36 +13,78 @@ import '../../constants.dart';
 class MatchDashboardScreen extends StatelessWidget {
   final Event event;
   final Team team;
-  const MatchDashboardScreen(
-      {required this.event, required this.team, super.key});
+  MatchDashboardScreen({required this.event, required this.team, super.key});
+
+  Widget getWidget() {
+    return Text("");
+  }
+
+  final List<Data> data = List.empty();
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: getMatchData(team, event),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        } else if (snapshot.hasError) {
-          return Text("${snapshot.error}");
-        }
+        future: getMatchDashboardSettings(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Text("${snapshot.error}");
+          }
 
-        final data = snapshot.data;
+          final data = snapshot.data;
 
-        if (data == null) {
-          return const Center(
-            child: Text(
-              "No data available",
-              style: smallerDefaultStyle,
-              textAlign: TextAlign.center,
-            ),
+          if (data == null) {
+            return const Center(
+              child: Text(
+                "No data available",
+                style: smallerDefaultStyle,
+                textAlign: TextAlign.center,
+              ),
+            );
+          }
+
+          /// Map the data to the Data class.
+          return FutureBuilder(
+            future: getMatchData(team, event),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+
+              final matchData = snapshot.data;
+
+              if (matchData == null) {
+                return const Center(
+                  child: Text(
+                    "No data available",
+                    style: smallerDefaultStyle,
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              }
+
+             /*  return ListView.builder(
+                itemCount: data.widgetNumber,
+                itemBuilder: (context, index) {
+                  return Text("");
+                },
+              ); */
+
+              return Text(matchData[0]
+                  .answers[data.dashboardWidgets[0].questionIndex]
+                  .toString());
+
+              //return PresetLineChart();
+              return Text(matchData.first.answers.toString());
+            },
           );
-        }
-        return PresetLineChart();
-        //return Text(data.first.answers.toString());
-      },
-    );
+        });
   }
 }
---
+++ lib/system/axis/Data.dart
@@ -0,0 +1,10 @@
+/// Written by Juan Pablo Gutiérrez
+/// 29 - 10 - 2023
+
+/// Represents the data for the dashboard widgets
+class Data {
+  final String name;
+  final int value;
+
+  Data(this.name, this.value);
+}
