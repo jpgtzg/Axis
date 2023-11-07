4dfce9a15a3a0908647a5adc5b9ca01ad39ba048
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-11-07 12:00:00 -0600

    Created PitDashboardScreen

--- lib/screens/pit_scouting/pit_dashboard_screen.dart
+++ lib/screens/pit_scouting/pit_dashboard_screen.dart
@@ -1,18 +1,108 @@
 /// Written by Juan Pablo Gutiérrez
 /// 02 01 2023
 
+import 'package:axis/constants.dart';
+import 'package:axis/system/axis/realm/realm_manager.dart';
+import 'package:axis/system/axis/realm/realm_models.dart';
 import 'package:axis/system/tba/event/event.dart';
 import 'package:axis/system/tba/team/team.dart';
 import 'package:flutter/material.dart';
 
-class PitDashboardScreen extends StatelessWidget {
+class PitDashboardScreen extends StatefulWidget {
   final Event event;
   final Team team;
   const PitDashboardScreen(
       {required this.team, required this.event, super.key});
 
   @override
+  State<PitDashboardScreen> createState() => _PitDashboardScreenState();
+}
+
+class _PitDashboardScreenState extends State<PitDashboardScreen> {
+  late Future<PitDashboardSchema?>? dashboardDataFuture;
+  late Future<List<PitDataSchema>> pitDataFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    dashboardDataFuture = getPitDashboardSettings();
+    dashboardDataFuture!.then((dashboardData) {
+      if (dashboardData != null) {
+        pitDataFuture = getPitData(widget.team, widget.event);
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return FutureBuilder(
+      future: dashboardDataFuture,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        }
+
+        final dashboardData = snapshot.data;
+
+        if (dashboardData == null) {
+          return const Center(
+            child: Text(
+              "No data available",
+              style: smallerDefaultStyle,
+              textAlign: TextAlign.center,
+            ),
+          );
+        }
+
+        return FutureBuilder(
+          future: pitDataFuture,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            }
+
+            final pitData = snapshot.data;
+
+            if (pitData == null || pitData.isEmpty) {
+              return const Center(
+                child: Text(
+                  "No data available yet",
+                  style: smallerDefaultStyle,
+                  textAlign: TextAlign.center,
+                ),
+              );
+            }
+
+            return SizedBox(
+              height: 600,
+              child: ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: dashboardData.widgetNumber,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      Text(dashboardData.dashboardWidgets[index].title),
+                      Text(pitData[0]
+                          .answers[dashboardData
+                              .dashboardWidgets[index].textData!.dataIndex]
+                          .value
+                          .toString()),
+                    ],
+                  );
+                },
+              ),
+            );
+          },
+        );
+      },
+    );
   }
 }
