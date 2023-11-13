c3fc86af3a4990bec72cc0f76ce8574c9b81a9ca
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-11-13 12:00:00 -0600

    Fixed AddButton

--- lib/widgets/settings/add_button.dart
+++ lib/widgets/settings/add_button.dart
@@ -1,34 +1,48 @@
 /// Written by Juan Pablo Gutiérrez
 
 import 'package:axis/constants.dart';
+import 'package:axis/screens/question_settings_screen.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
 class AddButton extends StatelessWidget {
-  const AddButton({super.key});
+  final int index;
+  final Origin origin;
+  const AddButton(
+      {super.key, required this.index, required this.origin});
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 13 / 4,
-      child: DottedBorder(
-        dashPattern: const [10],
-        borderType: BorderType.RRect,
-        radius: const Radius.circular(25),
-        color: paleteBlue,
-        strokeWidth: 3.5,
-        child: ClipRRect(
-          child: Container(
-            color: paleteBlue.withOpacity(0.2),
-            height: 150,
-            width: double.infinity,
-            child: IconButton(
-                icon: const Icon(
-                  Icons.add,
-                  size: 50,
-                  color: paleteBlue,
-                ),
-                onPressed: () {}),
+    return GestureDetector(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => QuestionSettingsScreen(
+            index: index,
+            origin: origin,
+          ),
+        ),
+      ),
+      child: AspectRatio(
+        aspectRatio: 13 / 4,
+        child: DottedBorder(
+          dashPattern: const [10],
+          borderType: BorderType.RRect,
+          radius: const Radius.circular(25),
+          color: paleteBlue,
+          strokeWidth: 3.5,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(25),
+            child: Container(
+              color: paleteBlue.withOpacity(0.2),
+              height: 150,
+              width: double.infinity,
+              child: const Icon(
+                Icons.add,
+                size: 50,
+                color: paleteBlue,
+              ),
+            ),
           ),
         ),
       ),
