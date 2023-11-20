f544e60a8a7005f5dedb10bed651e7c26200536d
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-11-20 14:35:41 -0600

    Adapted text box for new models

--- lib/widgets/text_box.dart
+++ lib/widgets/text_box.dart
@@ -2,56 +2,83 @@
 /// 08 11 2023
 
 import 'package:axis/constants.dart';
+import 'package:axis/system/axis/realm/realm_models.dart';
 import 'package:axis/widgets/standart_spacer.dart';
 import 'package:flutter/material.dart';
+import 'package:realm/realm.dart';
 
 class TextBox extends StatelessWidget {
   final List data;
-  final int dataIndex;
+  final TextWidgetData textWidgetData;
   final String title;
-  const TextBox(
-      {required this.data,
-      required this.dataIndex,
-      required this.title,
-      super.key});
+  final Future? formsFuture;
+
+  TextBox({
+    required this.data,
+    required this.textWidgetData,
+    required this.title,
+    required this.formsFuture,
+    Key? key,
+  }) : super(key: key);
+
+  Future<int?> getDataIndex() async {
+    final value = await formsFuture;
+    return value.questionsArray.indexWhere(
+        (element) => element.questionID == textWidgetData.dataIndex);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(25),
-        color: paleteBlack,
-      ),
-      padding: const EdgeInsets.only(
-        right: 24,
-        left: 24,
-        top: 24,
-        bottom: 24,
-      ),
-      child: Column(
-        children: [
-          Align(
-            alignment: Alignment.center,
-            child: Text(
-              title,
-              style: smallerDefaultStyle,
-            ),
+    return FutureBuilder<int?>(
+      future: getDataIndex(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        }
+        
+        final dataIndex = snapshot.data;
+
+        return Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25),
+            color: paleteBlack,
           ),
-          const StandardSpacer(height: standartSpacerHeight),
-          ...List.generate(
-            data.length,
-            (index) {
-              return Align(
+          padding: const EdgeInsets.only(
+            right: 24,
+            left: 24,
+            top: 24,
+            bottom: 24,
+          ),
+          child: Column(
+            children: [
+              Align(
                 alignment: Alignment.center,
                 child: Text(
-                  data[index].answers[dataIndex].value.toString(),
+                  title,
                   style: smallerDefaultStyle,
                 ),
-              );
-            },
+              ),
+              const StandardSpacer(height: standartSpacerHeight),
+              ...List.generate(
+                data.length,
+                (index) {
+                  return Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      data[index].answers[dataIndex].value.toString(),
+                      style: smallerDefaultStyle,
+                    ),
+                  );
+                },
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
